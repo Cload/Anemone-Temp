42b90287f4cbcaa223e567331b4fdd546e9c8b45
Michele <michele.beccari@outlook.com>
2020-03-13 15:29:16 +0100

    Improve DataHandling in MachinesDashBoard

--- src/app/Components/machines-dashboard/machines-dashboard.component.ts
+++ src/app/Components/machines-dashboard/machines-dashboard.component.ts
@@ -4,6 +4,7 @@ import { forkJoin } from 'rxjs';
 import { MachineDisplaySetupService } from 'src/app/Services/MachineDisplaySetup/machine-display-setup.service';
 import { MachineStatusesService } from 'src/app/Services/MachineStatuses/machine-statuses.service';
 import { MachinePanelSetupService } from 'src/app/Services/MachinePanelSetup/machine-panel-setup.service';
+import { IMachineStatus } from 'src/app/Objects/IMachineStatus';
 
 @Component({
   selector: 'app-machines-dashboard',
@@ -15,7 +16,7 @@ export class MachinesDashboardComponent implements OnInit {
 
   isLoading : boolean = true;
   machinePanelSetUp : number;
-  machineStatuses : any[];
+  machineStatuses : IMachineStatus[];
   machineDisplaySetUp : IMachineDisplaySetup;
 
   constructor(
@@ -27,20 +28,45 @@ export class MachinesDashboardComponent implements OnInit {
   ngOnInit(): void {
     let machinePanelObservable = this.machinePanelSetUpService.getMachinePanelSetUp();
     let machineStatusesObservable = this.machineStatusesService.getMachineStatuses();
-    let machineDisplayObservable = this.machineDisplaySetUpService.getMachineDisplaySetup(54);
+    let machineDisplayObservable = this.machineDisplaySetUpService.getMachineDisplaySetup(7);
     forkJoin(
       {
-        machinePanelSetUp : machinePanelObservable,
+        machinePanelSetup : machinePanelObservable,
         machineStatuses : machineStatusesObservable,
         machineDisplaySetUp : machineDisplayObservable
       }
     )
-    .subscribe(res => console.log(res))
+    .subscribe(res =>this.onDataRetrieved(res))
   }
 
   onDataRetrieved({machinePanelSetup, machineStatuses, machineDisplaySetUp} : 
                   {machinePanelSetup : number , machineStatuses : any[], machineDisplaySetUp : IMachineDisplaySetup})
   {
+    let formattedMachineStatuses : IMachineStatus[] = [];
+    machineStatuses.forEach(ms => 
+      {
+        let currentMachineStatus :  IMachineStatus= {};
+        Object.keys({...machineDisplaySetUp, ...ms}).forEach(key => 
+          {
+              let field = key.split('_');
+              field.forEach((value, index, array) => 
+                {
+                  field[index] = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase() //Maiuscolare la prima lettera 
+                });
+              if (ms[machineDisplaySetUp[key]])
+              {
+                currentMachineStatus[`${field.join('')}`] = ms[machineDisplaySetUp[key]]
+              } else if (machineDisplaySetUp[key])
+              {
+                currentMachineStatus[`${field.join('')}`] = machineDisplaySetUp[key];
+              } else if (ms[key]) {
+                currentMachineStatus[`${field.join('')}`] = ms[key];
+              }
+            
+          });
+        formattedMachineStatuses.push(currentMachineStatus)
+      });
+    this.machineStatuses = formattedMachineStatuses;
     this.isLoading = false;
   }
 
